ff9455c49e6e83f8129a4aac8d2b1bfca7dd3642
Mukesh Murugan <iammukeshm@gmail.com>
2021-02-26 22:39:35 +0530

    login request added

--- BlazorHero.CleanArchitecture.Application/Requests/Identity/LoginRequest.cs
+++ BlazorHero.CleanArchitecture.Application/Requests/Identity/LoginRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace BlazorHero.CleanArchitecture.Application.Requests.Identity
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorHero.CleanArchitecture.Application.Requests.Identity
 {
     public class LoginRequest
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
