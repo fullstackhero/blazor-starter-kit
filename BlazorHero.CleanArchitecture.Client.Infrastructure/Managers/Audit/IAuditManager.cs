83127211ff9bfb1c4ed6ca42c296d0ac2ddd0bcc
Mukesh Murugan <iammukeshm@gmail.com>
2021-04-05 06:43:30 +0530

    added audit trail UI

---
+++ BlazorHero.CleanArchitecture.Client.Infrastructure/Managers/Audit/IAuditManager.cs
@@ -0,0 +1,15 @@
+﻿using BlazorHero.CleanArchitecture.Application.Responses.Audit;
+using BlazorHero.CleanArchitecture.Shared.Wrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Infrastructure.Managers.Audit
+{
+    public interface IAuditManager : IManager
+    {
+        Task<IResult<IEnumerable<AuditResponse>>> GetCurrentUserTrailsAsync();
+    }
+}
