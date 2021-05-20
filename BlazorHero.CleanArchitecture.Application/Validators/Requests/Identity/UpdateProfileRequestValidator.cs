897d00ef16133ea76a420020b77e05dcef4f0f46
Chebotov Nikolay <spiritkola@hotmail.com>
2021-05-21 00:24:17 +0300

    update(FluentValidation): Add validation for Profile page

---
+++ BlazorHero.CleanArchitecture.Application/Validators/Requests/Identity/UpdateProfileRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using BlazorHero.CleanArchitecture.Application.Requests.Identity;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace BlazorHero.CleanArchitecture.Application.Validators.Requests.Identity
+{
+    public class UpdateProfileRequestValidator : AbstractValidator<UpdateProfileRequest>
+    {
+        public UpdateProfileRequestValidator(IStringLocalizer<UpdateProfileRequestValidator> localizer)
+        {
+            RuleFor(request => request.FirstName)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["First Name is required"]);
+            RuleFor(request => request.LastName)
+                .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["Last Name is required"]);
+        }
+    }
+}
