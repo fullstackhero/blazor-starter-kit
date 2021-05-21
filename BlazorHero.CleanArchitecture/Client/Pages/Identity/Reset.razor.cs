5bca51ca8d48277ce6c66839a86a1b9000429a1e
Chebotov Nikolay <spiritkola@hotmail.com>
2021-05-22 01:13:07 +0300

    update(FluentValidation): Add validation for Reset page

--- BlazorHero.CleanArchitecture/Client/Pages/Identity/Reset.razor.cs
+++ BlazorHero.CleanArchitecture/Client/Pages/Identity/Reset.razor.cs
@@ -2,36 +2,17 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.WebUtilities;
 using MudBlazor;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BlazorHero.CleanArchitecture.Client.Pages.Identity
 {
     public partial class Reset
     {
-        private bool success;
-        private string[] errors = { };
-        private MudForm form;
+        [Inject] private Microsoft.Extensions.Localization.IStringLocalizer<Reset> localizer { get; set; }
 
-        [Parameter]
-        [Required]
-        [EmailAddress]
-        public string Email { get; set; }
-
-        [Parameter]
-        [Required]
-        public string Password { get; set; }
-
-        [Parameter]
-        [Required]
-        public string ConfirmPassword { get; set; }
-
-        [Parameter]
-        public string Token { get; set; }
+        private readonly ResetPasswordRequest resetPasswordModel = new();
 
         protected override void OnInitialized()
         {
@@ -39,30 +20,25 @@ namespace BlazorHero.CleanArchitecture.Client.Pages.Identity
             if (QueryHelpers.ParseQuery(uri.Query).TryGetValue("Token", out var param))
             {
                 var queryToken = param.First();
-                Token = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(queryToken));
+                resetPasswordModel.Token = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(queryToken));
             }
         }
 
         private async Task SubmitAsync()
         {
-            if (!string.IsNullOrEmpty(Token))
+            if (!string.IsNullOrEmpty(resetPasswordModel.Token))
             {
-                form.Validate();
-                if (form.IsValid)
+                var result = await _userManager.ResetPasswordAsync(resetPasswordModel);
+                if (result.Succeeded)
                 {
-                    var request = new ResetPasswordRequest { Email = Email, Password = Password, Token = Token };
-                    var result = await _userManager.ResetPasswordAsync(request);
-                    if (result.Succeeded)
+                    _snackBar.Add(localizer[result.Messages[0]], Severity.Success);
+                    _navigationManager.NavigateTo("/");
+                }
+                else
+                {
+                    foreach (var message in result.Messages)
                     {
-                        _snackBar.Add(localizer[result.Messages[0]], Severity.Success);
-                        _navigationManager.NavigateTo("/");
-                    }
-                    else
-                    {
-                        foreach (var message in result.Messages)
-                        {
-                            _snackBar.Add(localizer[message], Severity.Error);
-                        }
+                        _snackBar.Add(localizer[message], Severity.Error);
                     }
                 }
             }
@@ -90,32 +66,6 @@ namespace BlazorHero.CleanArchitecture.Client.Pages.Identity
                 PasswordInputIcon = Icons.Material.Filled.Visibility;
                 PasswordInput = InputType.Text;
             }
-        }
-
-        private IEnumerable<string> PasswordStrength(string pw)
-        {
-            if (string.IsNullOrWhiteSpace(pw))
-            {
-                yield return "Password is required!";
-                yield break;
-            }
-            if (pw.Length < 8)
-                yield return "Password must be at least of length 8";
-            if (!Regex.IsMatch(pw, @"[A-Z]"))
-                yield return "Password must contain at least one capital letter";
-            if (!Regex.IsMatch(pw, @"[a-z]"))
-                yield return "Password must contain at least one lowercase letter";
-            if (!Regex.IsMatch(pw, @"[0-9]"))
-                yield return "Password must contain at least one digit";
-        }
-
-        private MudTextField<string> pwField;
-
-        private string PasswordMatch(string arg)
-        {
-            if (pwField.Value != arg)
-                return "Passwords don't match";
-            return null;
         }
     }
 }
