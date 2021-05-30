36afc5931d5d8dc8077e2a57c6e43d36f5ce44b1
Mohamed Salman <salman.primary@gmail.com>
2021-05-30 20:39:20 +0530

    Replace Permission generation using PermissionModule with Permission generation using Permissions class and refelection

--- BlazorHero.CleanArchitecture.Infrastructure/Helpers/ClaimExtensions.cs
+++ BlazorHero.CleanArchitecture.Infrastructure/Helpers/ClaimExtensions.cs
@@ -1,25 +1,34 @@
 ﻿using BlazorHero.CleanArchitecture.Application.Responses.Identity;
+using BlazorHero.CleanArchitecture.Infrastructure.Models.Identity;
 using BlazorHero.CleanArchitecture.Shared.Constants.Permission;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using BlazorHero.CleanArchitecture.Infrastructure.Models.Identity;
 
 namespace BlazorHero.CleanArchitecture.Infrastructure.Helpers
 {
     public static class ClaimsHelper
     {
-        public static void AddPermissions(this List<RoleClaimResponse> allPermissions, Type policy)
+        public static void GetAllPermissions(this List<RoleClaimResponse> allPermissions)
         {
-            FieldInfo[] fields = policy.GetFields(BindingFlags.Static | BindingFlags.Public);
-            foreach (FieldInfo fi in fields)
+            var modules = typeof(Permissions).GetNestedTypes();
+
+            foreach (var module in modules)
             {
-                allPermissions.Add(new RoleClaimResponse { Value = fi.GetValue(null)?.ToString(), Type = ApplicationClaimTypes.Permission, Group = policy.Name });
+                var fields = module.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy);
+
+                foreach (FieldInfo fi in fields)
+                {
+                    var propertyValue = fi.GetValue(null);
+
+                    if (propertyValue is not null)
+                        allPermissions.Add(new RoleClaimResponse { Value = fi.GetValue(null).ToString(), Type = ApplicationClaimTypes.Permission, Group = module.Name });
+                }
             }
+
         }
 
         public static async Task<IdentityResult> AddPermissionClaim(this RoleManager<BlazorHeroRole> roleManager, BlazorHeroRole role, string permission)
@@ -31,19 +40,6 @@ namespace BlazorHero.CleanArchitecture.Infrastructure.Helpers
             }
 
             return IdentityResult.Failed();
-        }
-
-        public static async Task GeneratePermissionClaimByModule(this RoleManager<BlazorHeroRole> roleManager, BlazorHeroRole role, string module)
-        {
-            var allClaims = await roleManager.GetClaimsAsync(role);
-            var allPermissions = PermissionModules.GeneratePermissionsForModule(module);
-            foreach (var permission in allPermissions)
-            {
-                if (!allClaims.Any(a => a.Type == ApplicationClaimTypes.Permission && a.Value == permission))
-                {
-                    await roleManager.AddClaimAsync(role, new Claim(ApplicationClaimTypes.Permission, permission));
-                }
-            }
         }
 
         public static async Task AddCustomPermissionClaim(this RoleManager<BlazorHeroRole> roleManager, BlazorHeroRole role, string permission)
