0b8cff120acf38063565ea9ca85dff4d7dd7d479
Chebotov Nikolay <spiritkola@hotmail.com>
2021-06-09 18:35:34 +0300

    update(DocumentExtendedAttribute): Add validator for adding the DocumentExtendedAttribute
    Add JsonValidator (for string properties)

---
+++ BlazorHero.CleanArchitecture.Application/Validators/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,13 @@
+﻿using BlazorHero.CleanArchitecture.Application.Interfaces.Serialization.Serializers;
+using FluentValidation;
+
+namespace BlazorHero.CleanArchitecture.Application.Validators.Extensions
+{
+    public static class ValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, string> MustBeJson<T>(this IRuleBuilder<T, string> ruleBuilder, IJsonSerializer jsonSerializer) where T : class
+        {
+            return ruleBuilder.SetValidator(new JsonValidator<T>(jsonSerializer));
+        }
+    }
+}
