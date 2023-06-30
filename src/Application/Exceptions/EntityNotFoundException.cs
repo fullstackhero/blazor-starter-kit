796702c2fa9727a046248ba20f81da1af7624bc7
MohammadMobasher <m.mobasher.z@gmail.com>
2023-06-30 11:18:29 +0330

    add EntityNotFoundException class

---
+++ src/Application/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BlazorHero.CleanArchitecture.Application.Exceptions
+{
+    [Serializable]
+    public class EntityNotFoundException : Exception
+    {
+        /// <summary>
+        /// Type of the entity.
+        /// </summary>
+        public Type EntityType { get; set; }
+
+        /// <summary>
+        /// Id of the Entity.
+        /// </summary>
+        public object Id { get; set; }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        public EntityNotFoundException()
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        public EntityNotFoundException(SerializationInfo serializationInfo, StreamingContext context)
+            : base(serializationInfo, context)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        public EntityNotFoundException(Type entityType, object id)
+            : this(entityType, id, null)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        public EntityNotFoundException(Type entityType, object id, Exception innerException)
+            : base($"There is no such an entity. Entity type: {entityType.FullName}, id: {id}", innerException)
+        {
+            EntityType = entityType;
+            Id = id;
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        public EntityNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="EntityNotFoundException"/> object.
+        /// </summary>
+        /// <param name="message">Exception message</param>
+        /// <param name="innerException">Inner exception</param>
+        public EntityNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
